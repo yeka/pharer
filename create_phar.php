a49b27afcd8297a9c4059357b4601057ba445823
Yakub Kristianto <yakub1986@gmail.com>
2013-09-18 22:39:51 +0700

    Inisial

---
+++ create_phar.php
@@ -0,0 +1,41 @@
+<?php
+if (count($argv) < 2) {
+
+} else {
+	$phar_file = $argv[1];
+	$src = getcwd().'/'.$argv[2];
+	createPhar($phar_file, $src); die;
+}
+
+function createPhar($phar_file, $src)
+{
+	$dir = readSrcDir($src);
+	if (empty($dir)) {
+		echo "Directory empty";
+		exit;
+	}
+	$phar = new Phar(__DIR__."/$phar_file", FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, basename($phar_file));
+	
+	foreach ($dir as $v) {
+		$phar[$v] = file_get_contents($src."/$v");
+	}
+	if (isset($dir['index.php'])) {
+		$phar->setStub($phar->createDefaultStub("index.php"));
+	}
+}
+
+function readSrcDir($src, $basedir = '')
+{
+	$dir = [];
+	$list = scandir($src.'/'.$basedir);
+	foreach ($list as $v) {
+		if (!trim($v, '.')) continue;
+		$path = $src.'/'.$basedir.$v;
+		if (is_dir($path)) {
+			$dir = array_merge($dir, readSrcDir($src, $basedir.$v.'/'));
+		} else {
+			$dir[] = $basedir.$v;
+		}
+	}
+	return $dir;
+}
